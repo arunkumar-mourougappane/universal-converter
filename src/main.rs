4e7811614d8b8fe4a61a7ed1410d9350d9d90336
Arunkumar Mourougappane <amouroug@buffalo.edu>
2024-08-11 16:05:19 -0500

    Add dropbox options

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@ use std::process::exit;
 use base64::{prelude::BASE64_STANDARD, Engine};
 use slint::{ComponentHandle, SharedString};
 
+
 slint::include_modules!();
 
 fn main() -> Result<(), slint::PlatformError> {
@@ -16,7 +17,7 @@ fn main() -> Result<(), slint::PlatformError> {
     ui.on_convert_text(move |text_data| {
         let ui = ui_handle.unwrap();
         let my_data = BASE64_STANDARD.encode(text_data.as_str());
-        ui.set_base64_text(SharedString::from(my_data.as_str()));
+        ui.set_output_text(SharedString::from(my_data.as_str()));
     });
 
     ui.run()
